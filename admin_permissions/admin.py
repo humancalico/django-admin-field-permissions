a23f7627daf7c90c75f21c2edf2e5f2ab5c32772
Akshat Agarwal <hey@akshat.dev>
2022-04-12 19:09:45 +0530

    add custom view and change permissions

--- admin_permissions/admin.py
+++ admin_permissions/admin.py
@@ -2,33 +2,60 @@
 
 from __future__ import unicode_literals
 
+import enum
+
+
+class PermissionType(enum.Enum):
+    VIEW = 1,
+    CHANGE = 2,
+
+
+def get_permission_type(perm_string):
+    # FIXME also raise the same exception if unable to split or index
+    # perm_string would be of the format can_change_somefield or can_view_somefield
+    if perm_string.split("_")[1] == "change":
+        return PermissionType.CHANGE
+    elif perm_string.split("_")[1] == "view":
+        return PermissionType.VIEW
+    else:
+        raise Exception(
+            "Illegal format to create permission. Please create them in the following format: can_change_* or can_view_*")
+
+
+def get_other_permission_type(perm_string):
+    if get_permission_type(perm_string) == PermissionType.VIEW:
+        return f'can_change_{perm_string.removeprefix("can_view_")}'
+    elif get_permission_type(perm_string) == PermissionType.CHANGE:
+        return f'can_view_{perm_string.removeprefix("can_change_")}'
+
 
 class FieldPermissionMixin(object):
     """Adds simple functionality to check the permissions for the fields"""
     fields_permissions = {}
-    fields_permissions_read_only = False
 
     def get_fieldsets(self, request, obj=None):
-        fieldsets = super(FieldPermissionMixin, self).get_fieldsets(request, obj)
+        fieldsets = super(FieldPermissionMixin, self).get_fieldsets(request, obj).copy()
 
-        if not self.fields_permissions_read_only:
-            for permission, fields in self.fields_permissions.items():
-                if not request.user.has_perm(permission):
-                    if isinstance(fields, (str, list)):
-                        fields = (fields,)
-                    fieldsets = self.remove_fields(fieldsets, *fields)
+        for permission, fields in self.fields_permissions.items():
+            if (not request.user.has_perm(permission)) or (
+                    not request.user.has_perm(get_other_permission_type(permission))):
+                if isinstance(fields, (str, list)):
+                    fields = (fields,)
+                fieldsets = self.remove_fields(fieldsets, *fields)
 
         return fieldsets
 
     def get_readonly_fields(self, request, obj=None):
-        fieldsets = super(FieldPermissionMixin, self).get_readonly_fields(request, obj)
+        fieldsets = super(FieldPermissionMixin, self).get_readonly_fields(request, obj).copy()
 
-        if self.fields_permissions_read_only:
-            for permission, fields in self.fields_permissions.items():
-                if not request.user.has_perm(permission):
-                    if isinstance(fields, (str, list)):
-                        fields = (fields,)
-                    fieldsets += fields
+        # if user has view perm but not corresponding change perm
+        read_only_perms = filter(lambda elem: get_permission_type(elem[0]) == PermissionType.VIEW,
+                                 self.fields_permissions)
+        for permission, fields in read_only_perms.items():
+            if request.user.has_perm(permission) and (not request.user.has_perm(get_other_permission_type(permission))):
+                if isinstance(fields, (str, list)):
+                    fields = (fields,)
+                fieldsets += fields
 
         return fieldsets
 
